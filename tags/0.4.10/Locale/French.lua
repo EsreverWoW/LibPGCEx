aa53bea9b2890e1387a035b18f3306373f8fe1fa
baanano <baanano@1275f3e0-ac1c-4fc8-8497-e48f2653dcdf>
2013-06-25 21:53:53 +0000

    Tagging as 0.4.10

---
+++ tags/0.4.10/Locale/French.lua
@@ -0,0 +1,7 @@
+﻿local _, InternalInterface = ...
+
+InternalInterface.Localization.RegisterLocale("French",--@do-not-package@
+{
+}--@end-do-not-package@
+--@localization(locale="frFR", format="lua_table", handle-subnamespaces="concat", namespace-delimiter="/")@
+)
---
+++ tags/0.4.10/Locale/German.lua
@@ -0,0 +1,7 @@
+﻿local _, InternalInterface = ...
+
+InternalInterface.Localization.RegisterLocale("German",--@do-not-package@
+{
+}--@end-do-not-package@
+--@localization(locale="deDE", format="lua_table", handle-subnamespaces="concat", namespace-delimiter="/")@
+)

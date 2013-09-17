ffcbd348b34ea0c0f6565a6f4af645c3484f53d1
baanano <baanano@1275f3e0-ac1c-4fc8-8497-e48f2653dcdf>
2013-09-17 19:02:46 +0000

    Tagging as 0.4.12

---
+++ tags/0.4.12/Locale/Korean.lua
@@ -0,0 +1,7 @@
+﻿local _, InternalInterface = ...
+
+InternalInterface.Localization.RegisterLocale("Korean",--@do-not-package@
+{
+}--@end-do-not-package@
+--@localization(locale="koKR", format="lua_table", handle-subnamespaces="concat", namespace-delimiter="/")@
+)
